47843f0265921044b5b826d9241372370d74ce46
AwesomePH <shidongxing@foxmail.com>
2019-02-17 11:02:15 +0800

    testing the type conversions in C

---
+++ variables&arithmetic/signed_to_unsigned_example.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int i;
+	unsigned int stop_val;
+
+	stop_val = 0;
+	i = -10;
+
+	while (i - stop_val>0) {    // no warning, but using '<' may cause a warning
+		printf("%d\n", i);
+		i = i + 1;
+	}
+	exit(EXIT_SUCCESS);
+}
+
+// we get:
+// -10 -9 ... -1
+
+// Note:
+// writting : while (i >= stop_val) may cause a warning on the unsigned and signed
